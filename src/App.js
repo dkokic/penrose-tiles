9e0cd46332bd8cdf04e0e7af6f7863ad9213b5f7
Draško Kokić <drasko.kokic@gmail.com>
2017-04-02 20:05:03 +0200

    Add react-motion to animate zoom-in

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Motion, spring } from 'react-motion'
 import logo from './logo.svg';
 import './App.css';
 import Pentagon from "./Pentagon";
@@ -11,7 +12,6 @@ class App extends Component {
     width: 1280,
     height: 600
   }
-  angle = -90
 
   componentDidMount() {
     [1, 2, 3, 4].forEach((i) => setTimeout(() => this.setState({ depth: i }), 1000 * i))
@@ -25,19 +25,27 @@ class App extends Component {
           <h2>Penrose Tiles in React</h2>
         </div>
         <div className="App-content">
-          <svg ref="svg" style={{border: "1px solid lightgray"}}
-               width="95%" height={this.svg.height}
-               preserveAspectRatio="xMidYMid"
-               viewBox={`0 0 ${this.svg.height} ${this.svg.height}`}>
-            <g transform={`translate(${this.svg.height / 2} ${this.svg.height / 2}) rotate(${this.angle})`}>
-              {
-                [-2, -1, 0, 1, 2].map((i) => (<line key={`y${i}`} stroke="black" x1={i * 100} y1="-300" x2={i * 100} y2="300" />))
-              }{
-                [-2, -1, 0, 1, 2].map((i) => (<line key={`x${i}`} stroke="black" x1="-300" y1={i * 100} x2="300" y2={i * 100} />))
-              }
-              <Pentagon depth={this.state.depth} />
-            </g>
-          </svg>
+          <Motion defaultStyle={{ angle: 0, scale: 1 }}
+                  style={{ angle: spring(-90, { stiffness: 60, damping: 17 }), scale: spring(1 + this.state.depth, { stiffness: 60, damping: 17 })}}
+                  onRest={() => { console.log('finished') }}>
+            { ({angle, scale}) =>
+              <svg ref="svg" style={{border: "1px solid lightgray"}}
+                   width="95%" height={this.svg.height}
+                   preserveAspectRatio="xMidYMid"
+                   viewBox={`0 0 ${this.svg.height} ${this.svg.height}`}>
+                <g transform={`translate(${this.svg.height / 2} ${this.svg.height / 2}) scale(${scale}) rotate(${angle})`}>
+                  {
+                    [-2, -1, 0, 1, 2].map((i) =>
+                      <line key={`y${i}`} stroke="black" x1={i * 100} y1="-300" x2={i * 100} y2="300"/>)
+                  }{
+                    [-2, -1, 0, 1, 2].map((i) =>
+                      <line key={`x${i}`} stroke="black" x1="-300" y1={i * 100} x2="300" y2={i * 100}/>)
+                  }
+                  <Pentagon depth={this.state.depth}/>
+                </g>
+              </svg>
+            }
+          </Motion>
         </div>
         <div className="App-footer">
           <span>Created by </span>
